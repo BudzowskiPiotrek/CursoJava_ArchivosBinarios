d29d05d76ffcf7e0e3f26c60d4bef5499acd0b6c
Piotrek Budzowski <piobudzows@gmail.com>
2025-05-12 14:05:11 +0200

    Update Libro.java

--- Actividad2/Libro.java
+++ Actividad2/Libro.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class Libro implements Serializable {
     private static final long serialVersionUID = 1L;
+    public static int contador = 0;
+    private int id;
     private String titulo;
     private String autor;
     private int anio;
@@ -14,6 +16,16 @@ public class Libro implements Serializable {
         this.autor = autor;
         this.anio = anio;
         this.genero = genero;
+        this.id = contador;
+        contador++;
+    }
+
+    public static void setContador(int contador) {
+        Libro.contador = contador;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setTitulo(String titulo) {
@@ -30,6 +42,14 @@ public class Libro implements Serializable {
 
     public void setGenero(Genero genero) {
         this.genero = genero;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public static long getSerialversionuid() {
@@ -53,15 +73,10 @@ public class Libro implements Serializable {
     }
 
     public void verDatos() {
+        System.out.println("Id: " + id);
         System.out.println("Titulo: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("Año: " + anio);
         System.out.println("Genero: " + genero);
     }
-
-    @Override
-    public String toString() {
-        return "Libro [titulo=" + titulo + ", autor=" + autor + ", anio=" + anio + ", genero=" + genero + "]";
-    }
-
 }
